04aeea1f4066da002c56b2e91bffd3d065fa2fb4
gmenti <mentifg@gmail.com>
2017-08-02 11:14:20 -0300

    Add method to getmarketsummary by market name

--- src/main/java/com/trader/services/bittrex/BittrexService.java
+++ src/main/java/com/trader/services/bittrex/BittrexService.java
@@ -10,10 +10,13 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class BittrexService {
     private final static String BASE_URI = "https://bittrex.com/api/";
-    private final static String GET_MARKETS_URL = BASE_URI + "v1.1/public/getmarkets";
-    private final static String GET_MARKET_SUMMARIES_URL = BASE_URI + "v1.1/public/getmarketsummaries";
-    private final static String GET_CURRENCIES_URL = BASE_URI + "v1.1/public/getcurrencies";
-    private final static String GET_TICKER_URL = BASE_URI + "v1.1/public/getticker";
+    private final static String V1_URI = BASE_URI + "v1.1/";
+    private final static String V1_PUBLIC_URI = V1_URI + "public/";
+    private final static String GET_MARKETS_URL = V1_PUBLIC_URI + "getmarkets";
+    private final static String GET_MARKET_SUMMARY_URL = V1_PUBLIC_URI + "getmarketsummary";
+    private final static String GET_MARKET_SUMMARIES_URL = V1_PUBLIC_URI + "getmarketsummaries";
+    private final static String GET_CURRENCIES_URL = V1_PUBLIC_URI + "getcurrencies";
+    private final static String GET_TICKER_URL = V1_PUBLIC_URI + "getticker";
 
     private final RestTemplate restTemplate;
 
@@ -21,8 +24,19 @@ public class BittrexService {
         this.restTemplate = new RestTemplate();
     }
 
+    private String addMarketParamToUrl(String url, String market) {
+        return url + "?market=" + market;
+    }
+
     public MarketsResponse getMarkets() {
         return restTemplate.getForObject(GET_MARKETS_URL, MarketsResponse.class);
+    }
+
+    public MarketSummariesResponse getMarketSummary(String market) {
+        return restTemplate.getForObject(
+                addMarketParamToUrl(GET_MARKET_SUMMARY_URL, market),
+                MarketSummariesResponse.class
+        );
     }
 
     public MarketSummariesResponse getMarketSummaries() {
@@ -34,6 +48,9 @@ public class BittrexService {
     }
 
     public TickerResponse getTicker(String market) {
-        return restTemplate.getForObject(GET_TICKER_URL + "?market=" + market, TickerResponse.class);
+        return restTemplate.getForObject(
+                addMarketParamToUrl(GET_TICKER_URL, market),
+                TickerResponse.class
+        );
     }
 }
