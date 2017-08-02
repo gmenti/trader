6fd65967f378fab71c3a933819e69747f99bb312
gmenti <mentifg@gmail.com>
2017-08-02 11:37:23 -0300

    Add method to getmarkethistory by market name

--- src/main/java/com/trader/services/bittrex/BittrexService.java
+++ src/main/java/com/trader/services/bittrex/BittrexService.java
@@ -1,9 +1,6 @@
 package com.trader.services.bittrex;
 
-import com.trader.services.bittrex.responses.CurrenciesResponse;
-import com.trader.services.bittrex.responses.MarketSummariesResponse;
-import com.trader.services.bittrex.responses.MarketsResponse;
-import com.trader.services.bittrex.responses.TickerResponse;
+import com.trader.services.bittrex.responses.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,6 +14,7 @@ public class BittrexService {
     private final static String GET_MARKET_SUMMARIES_URL = V1_PUBLIC_URI + "getmarketsummaries";
     private final static String GET_CURRENCIES_URL = V1_PUBLIC_URI + "getcurrencies";
     private final static String GET_TICKER_URL = V1_PUBLIC_URI + "getticker";
+    private final static String GET_MARKET_HISTORY_URL = V1_PUBLIC_URI + "getmarkethistory";
 
     private final RestTemplate restTemplate;
 
@@ -51,6 +49,13 @@ public class BittrexService {
         return restTemplate.getForObject(
                 addMarketParamToUrl(GET_TICKER_URL, market),
                 TickerResponse.class
+        );
+    }
+
+    public MarketHistoryResponse getMarketHistory(String market) {
+        return restTemplate.getForObject(
+                addMarketParamToUrl(GET_MARKET_HISTORY_URL, market),
+                MarketHistoryResponse.class
         );
     }
 }
---
+++ src/main/java/com/trader/services/bittrex/responses/MarketHistoryResponse.java
@@ -0,0 +1,9 @@
+package com.trader.services.bittrex.responses;
+
+import com.trader.services.bittrex.objects.MarketHistory;
+
+import java.util.ArrayList;
+
+public class MarketHistoryResponse extends Response<ArrayList<MarketHistory>> {
+    //
+}
