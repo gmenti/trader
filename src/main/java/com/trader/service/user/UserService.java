41319f315c41cf317bc6577470a488f8e1c9280c
gmenti <mentifg@gmail.com>
2017-08-23 16:33:18 -0300

    Add method to save, create and get user token by email and password

--- src/main/java/com/trader/service/user/UserService.java
+++ src/main/java/com/trader/service/user/UserService.java
@@ -1,5 +1,6 @@
 package com.trader.service.user;
 
+import com.trader.service.user.exception.InvalidCredentialsException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,5 +9,26 @@ public class UserService {
 
     UserService(UserRepository repository) {
         this.repository = repository;
+    }
+
+    public User save(User user) {
+        return this.repository.save(user);
+    }
+
+    public User create(String email, String password) {
+        return this.save(new User(email, password));
+    }
+
+    public String getToken(String email, String password) throws InvalidCredentialsException {
+        User user = this.repository.getByEmail(email);
+
+        if (user != null && user.checkPassword(password)) {
+            user.generateToken();
+            this.save(user);
+
+            return user.getToken();
+        }
+
+        throw new InvalidCredentialsException();
     }
 }
