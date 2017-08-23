ab9b1048ae7ddcb6828f2d33c74078358ca242b4
gmenti <mentifg@gmail.com>
2017-08-23 14:21:12 -0300

    Add user repository

---
+++ src/main/java/com/trader/service/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.trader.service.user;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface UserRepository extends CrudRepository<User, Long> {
+    //
+}
