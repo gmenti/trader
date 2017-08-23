b9627a955254a2dbb661ae6b4cbc1e54f34ad906
gmenti <mentifg@gmail.com>
2017-08-23 16:31:43 -0300

    Add token field/generator and password encrypter

--- src/main/java/com/trader/service/user/User.java
+++ src/main/java/com/trader/service/user/User.java
@@ -2,13 +2,16 @@ package com.trader.service.user;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.UUID;
 
 @Entity
 @Table(uniqueConstraints = {
     @UniqueConstraint(columnNames = {"email"}),
+    @UniqueConstraint(columnNames = {"token"}),
 })
 public class User {
     @Id
@@ -16,12 +19,16 @@ public class User {
     private long id;
 
     @NotNull
+    @Length(max = 100)
+    private String token;
+
+    @NotNull
     @Email
     @Length(min = 3, max = 255)
     private String email;
 
     @NotNull
-    @Length(min = 8, max = 50)
+    @Length(max = 255)
     private String password;
 
     protected User() {
@@ -31,10 +38,15 @@ public class User {
     User(String email, String password) {
         this.setEmail(email);
         this.setPassword(password);
+        this.generateToken();
     }
 
     public long getId() {
         return id;
+    }
+
+    public String getToken() {
+        return token;
     }
 
     public String getEmail() {
@@ -45,11 +57,15 @@ public class User {
         this.email = email;
     }
 
-    public String getPassword() {
-        return password;
+    public void setPassword(String password) {
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public boolean checkPassword(String password) {
+        return BCrypt.checkpw(password, this.password);
+    }
+
+    public void generateToken() {
+        this.token = UUID.randomUUID().toString();
     }
 }
