c64612d2fbcb46c4c18cfa7d4ce1fd13df5df380
gmenti <mentifg@gmail.com>
2017-08-25 15:36:28 -0300

    Add cors configuration

---
+++ src/main/java/com/trader/http/HttpConfigurer.java
@@ -0,0 +1,16 @@
+package com.trader.http;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@EnableWebMvc
+@Configuration
+public class HttpConfigurer extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("*").allowedOrigins("*");
+    }
+}
